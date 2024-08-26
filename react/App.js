26869e0bf90deebc7a249a3cb57ac8b1da1ea271
SBDEV-FR <115069416+SBDEV-FR@users.noreply.github.com>
2024-08-26 12:13:06 +0200

    Add files via upload
    task 2 react

---
+++ react/App.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import Counter from './counter'; // Import the Counter component
+
+function App() {
+  return (
+    <div> <Counter /> </div>);
+}
+
+export default App;
